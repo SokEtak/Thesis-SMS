532673c3884737887816e4171c68eb01f65a318d
Sok Etak <taktakcars123467@gmail.com>
2026-03-01 08:48:04 +0700

    add shortcut folder

--- start-laravel.bat
+++ start-laravel.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-cd /d "C:\Users\Vanntak\Herd\Thesis-SMS-Server"
+cd /d "C:\Users\Vanntak\Herd\Thesis-SMS"
 
 :: Open VS Code in project
-start "" code "C:\Users\Vanntak\Herd\Thesis-SMS-Server"
+start "" code "C:\Users\Vanntak\Herd\Thesis-SMS"
 
 :: Build the project
 call npm run build
 
 :: Open Windows Terminal with correct working directory (no extra default shell)
 start "" wt ^
-new-tab --title "VITE DEV (LAN 5174)" -d "C:\Users\Vanntak\Herd\Thesis-SMS-Server" cmd /k "npm run dev -- --host 0.0.0.0 --port 5174" ^
-new-tab --title "REVERB" -d "C:\Users\Vanntak\Herd\Thesis-SMS-Server" cmd /k "php artisan reverb:start" ^
-new-tab --title "LARAVEL SERVE" -d "C:\Users\Vanntak\Herd\Thesis-SMS-Server" cmd /k "php artisan serve"
+new-tab --title "VITE DEV (LAN 5174)" -d "C:\Users\Vanntak\Heis-SMS" cmd /k "npm run dev -- --host 0.0.0.0 --port 5174" ^
+new-tab --title "REVERB" -d "C:\Users\Vanntak\Hsis-SMS" cmd /k "php artisan reverb:start" ^
+new-tab --title "LARAVEL SERVE" -d "C:\Users\Vanntak\esis-SMS" cmd /k "php artisan serve"
